65e160512a924b3a1538960d22231b18f12efb95
James Littlejohn <jlittlej@ca.ibm.com>
2017-05-01 16:08:06 -0400

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 class HelloWorld
+<<<<<<< Upstream, based on 5ba47592254776549e943759118d687ad9cad70f
 //test edit from github 20170501_1550
+=======
+>>>>>>> 8eae76e test
 //test edit from github 20170501_1538
 {
    public static void main(String args[])
